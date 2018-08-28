d9192935527b0f76d33f23c6bc3b4bd8644623d2
Harshit <harshitacharya.kadekaru@itcinfotech.com>
2018-08-29 00:06:21 +0530

    first commit

---
+++ ngApp/src/app/auth.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+//mongo DB
+ private _registerUrl="http://localhost:3000/api/register"
+  //Mysql
+  //private _registerUrl="http://localhost:8080/api/register"
+
+  private _loginUrl="http://localhost:3000/api/login"
+
+  constructor(private http: HttpClient,
+              private router: Router) { }
+
+  registerUser(user){
+       return this.http.post<any>(this._registerUrl, user);
+  }
+
+  loginUser(user){
+    return this.http.post<any>(this._loginUrl, user)
+  }
+
+  loggedIn(){
+    return !!localStorage.getItem('token');
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
+
+  logoutUser(){
+    localStorage.removeItem('token');
+    this.router.navigate(['/events'])
+  }
+}
